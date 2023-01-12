f68c6059687517467512f9d148afe70c1da8c1d2
Mayowa Ajiboye <mayaajiboye@gmail.com>
2023-01-12 14:24:57 -0500

    manipulate HTML using JS

--- 02_DOM_Manipulation/src/index.js
+++ 02_DOM_Manipulation/src/index.js
@@ -1,23 +1,47 @@
-//BookStore has been moved to data.js 
-console.log(bookStore);
+//BookStore has been moved to data.js
+// console.log(bookStore);
 
 function formatPrice(price) {
   // return `$${price.toFixed(2)}`;
   return '$' + Number.parseFloat(price).toFixed(2);
 }
 
-// create a function called renderBook(book)
-// it will take a book object as an argument
-// and create the html struture for rendering 
-// that book and insert it into our webpage!
+function renderHeader(bookStore){
+  document.querySelector('header h1').textContent = bookStore.name
+}
+renderHeader(bookStore)
 
-// function renderBook(book) {
-// should create an li element that looks something like this:
-  //   <li class="list-li">
-  //     <h3>Eloquent JavaScript</h3>
-  //     <p>Marjin Haverbeke</p>
-  //     <p>$10.00</p>
-  //     <img src="https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" alt="Eloquent JavaScript cover"/>
-  //   </li>
+function renderFooter(bookStore){
+  document.querySelector('#store').textContent = bookStore.location
+  document.querySelector('#number').textContent = bookStore.number
+  document.querySelector('#address').textContent = bookStore.address
+}
+renderFooter(bookStore)
 
-  
+function renderBook(book){
+  const li = document.createElement('li');
+  // add class to list element
+  li.className = 'list-li'
+  const h3 = document.createElement('h3');
+  h3.textContent = book.title
+  const pAuthor = document.createElement('p')
+  pAuthor.textContent = `by ${book.author}`
+  const pPrice = document.createElement('p')
+  pPrice.textContent = formatPrice(book.price)
+  const img = document.createElement('img')
+  img.src = book.imageUrl;
+  img.alt = `${book.title} cover`
+
+  //append element to the DOM (so we can see it)
+  const list = document.querySelector('#book-list')
+  li.append(h3, pAuthor,pPrice, img);
+  list.append(li);
+}
+
+// bookStore.inventory.forEach(book => renderBook(book))
+bookStore.inventory.forEach(renderBook)
+
+
+function removeBook(index) {
+  document.querySelectorAll('li.list-li')[index].remove()
+}
